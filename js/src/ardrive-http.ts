f5eb45c6cfa6290bad635a2db79aee2fc3b66d1b
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-02-15 14:57:44 -0300

    remove timeout

--- js/src/ardrive-http.ts
+++ js/src/ardrive-http.ts
@@ -81,7 +81,6 @@ const get = async ([url, responseType, retries, retryDelayMs, noLogs = false, re
     const response = await fetch(url, {
       method: 'GET',
       redirect: 'follow',
-      signal: AbortSignal.timeout(8000), // 8s timeout
     });
 
     const statusCode = response.status;
@@ -138,7 +137,6 @@ const post = async ([
       },
       redirect: 'follow',
       body: data,
-      signal: AbortSignal.timeout(8000), // 8s timeout
     });
 
     const statusCode = response.status;
